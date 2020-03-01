50353a53f7649a5177d0adcb2e253031b47cbf36
seamo88 <bstockgame.88@gmail.com>
2020-03-01 09:23:09 -0600

    Add project files.

---
+++ 5_2_Lab_sean/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5_2_Lab_sean
+{
+    class Program
+    { 
+         public enum WeekDays
+         {
+
+        Moday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+         }
+
+
+
+
+
+    
+        static void Main(string[] args)
+        {
+             //Create a list to hold tasks
+            List<string> tasklist = new List<string>();
+
+            //for each value in the Enum...let the user create a task.
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for" + day);
+                string task = day + ":" + Console.ReadLine();
+                tasklist.Add(task);
+
+            }
+
+            //print all of the tasks.
+            foreach(string task in tasklist)
+            {
+                Console.WriteLine(task);
+            }
+
+
+        }
+    }
+}
